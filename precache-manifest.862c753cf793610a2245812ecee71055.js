93cd8665c74e4ce7b3f73040f67746e8adaaf60f
Savitha Gollamudi <savitha.gollamudi@gmail.com>
2020-10-21 18:58:04 +0530

    Updates

---
+++ precache-manifest.862c753cf793610a2245812ecee71055.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9ecad91bde9746dc3f8f823dd3da05aa",
+    "url": "/Deck-Of-Cards/index.html"
+  },
+  {
+    "revision": "1c13a7dedeb687cece8e",
+    "url": "/Deck-Of-Cards/static/css/main.d0e2aac3.chunk.css"
+  },
+  {
+    "revision": "bf49450fcb8579e0970b",
+    "url": "/Deck-Of-Cards/static/js/2.cb4136f4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Deck-Of-Cards/static/js/2.cb4136f4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1c13a7dedeb687cece8e",
+    "url": "/Deck-Of-Cards/static/js/main.d99270eb.chunk.js"
+  },
+  {
+    "revision": "65ee1f127fe2abcc1afe",
+    "url": "/Deck-Of-Cards/static/js/runtime-main.4d65aba0.js"
+  }
+]);
